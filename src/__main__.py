c2189f40fabd310b504d334a14613ff69fee8648
vcokltfre <vcokltfre@gmail.com>
2022-01-14 02:06:36 +0000

    fix: deploy to nextcord and disnake servers

--- src/__main__.py
+++ src/__main__.py
@@ -12,7 +12,7 @@ def main() -> None:
     intents.members = True
     intents.guild_messages = True
 
-    bot = Bot(intents=intents, text_guilds=[683009037830324235])
+    bot = Bot(intents=intents, text_guilds=[881118111967883295, 808030843078836254])
 
     for ext in [
         "src.exts.errors",
