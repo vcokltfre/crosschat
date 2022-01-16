e45306e5bd89d843fb349638d6771a5499786848
vcokltfre <vcokltfre@gmail.com>
2022-01-16 19:39:22 +0000

    feat: improve error messages for owners

--- src/exts/errors.py
+++ src/exts/errors.py
@@ -1,3 +1,5 @@
+from traceback import format_exc
+
 from disnake import Interaction
 from disnake.ext.commands import CheckFailure, Cog, MissingPermissions, NotOwner
 
@@ -5,6 +7,9 @@ from src import Bot
 
 
 class ErrorHandler(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
     @Cog.listener()
     async def on_slash_command_error(self, itr: Interaction, error: Exception) -> None:
         await self.handle_interaction_error(itr, error)
@@ -21,9 +26,15 @@ class ErrorHandler(Cog):
         if isinstance(error, (MissingPermissions, NotOwner, CheckFailure)):
             await itr.send(f"You don't have the required permissions to do that.", ephemeral=True)
         else:
-            await itr.send(f"An unknown error occurred while running the command :(", ephemeral=True)
+            if await self.bot.is_owner(itr.author):
+                await itr.send(
+                    f"An error occurred while processing the interaction:\n```py\n{format_exc(limit=1900)}```",
+                    ephemeral=True,
+                )
+            else:
+                await itr.send(f"An unknown error occurred while running the command :(", ephemeral=True)
             raise error
 
 
 def setup(bot: Bot) -> None:
-    bot.add_cog(ErrorHandler())
+    bot.add_cog(ErrorHandler(bot))
